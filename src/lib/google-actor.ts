9101f697d0492a5cb86fa682b3b7e5ce1bb3a6c5
Josh Wulf <josh@magikcraft.io>
2020-09-25 01:00:12 +1200

    replace newlines in private key
    Signed-off-by: Josh Wulf <josh@magikcraft.io>

--- src/lib/google-actor.ts
+++ src/lib/google-actor.ts
@@ -34,7 +34,7 @@ async function openSpreadsheet(docId) {
   await spreadsheet
     .useServiceAccountAuth({
       client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY,
+      private_key: process.env.GOOGLE_PRIVATE_KEY?.split("\\n").join("\n"),
     })
     .catch(console.log);
   await spreadsheet.loadInfo().catch(console.log);
